17ef8a4046b6abb143e7f22c62e586c5bdbbbecb
wpichl <walter.pichl@hotmail.com>
2021-04-22 23:40:40 +0200

    feat: add small website

---
+++ src/esp32cam/src/main.cpp
@@ -0,0 +1,31 @@
+#include "common_includes.h"
+#include "WiFiHandler/WiFiHandler.h"
+#include "config.h"
+
+AsyncWebServer server(80);
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+<title>Planters</title>
+</head><body>
+<h1>test</h1>
+</body></html>)rawliteral";
+
+
+void setup()
+{
+	Serial.begin(9600);
+	WiFiHandler wh = WiFiHandler(config::ssid, config::password);
+	wh.Dump();
+	wh.Connect();
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+	{
+		request->send_P(200, "text/html", index_html);
+	});
+
+	server.begin();
+}
+
+void loop()
+{
+}
