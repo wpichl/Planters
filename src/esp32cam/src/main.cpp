e8225e6f5af1bb9991cbf4641487f0a7c6bc89bc
wpichl <walter.pichl@hotmail.com>
2021-04-27 20:22:13 +0200

    fix: changed everything to c++ and vs code

--- src/esp32cam/src/main.cpp
+++ src/esp32cam/src/main.cpp
@@ -1,35 +1,13 @@
-#include "common_includes.h"
-#include "WiFiHandler/WiFiHandler.h"
-#include "config.h"
-
-AsyncWebServer server(80);
-
-/*
- * Data to send to the client
- * Ghetto fixed it. Don't use it for serious purpose.
- */
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-<title>Planters</title>
-</head><body>
-<h1>test</h1>
-</body></html>)rawliteral";
-
-
-void setup()
-{
+#include "config.hpp"
+#include "WiFiHandler.hpp"
+#include "common_includes.hpp"
+void setup() {
     Serial.begin(9600);
-    WiFiHandler wh = WiFiHandler(config::ssid, config::password);
+    WiFiHandler wh(config::ssid, config::password);
     wh.Dump();
     wh.Connect();
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        request->send_P(200, "text/html", index_html);
-    });
-
-    server.begin();
 }
 
-void loop()
-{
+void loop() {
+  // put your main code here, to run repeatedly:
 }
