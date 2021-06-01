1adfdc76598ea4edb94f93f3ca19e5ce8a57b301
wpichl <walter.pichl@hotmail.com>
2021-06-01 10:48:55 +0200

    feat: add jsonobject sender

--- src/esp32cam/src/main.cpp
+++ src/esp32cam/src/main.cpp
@@ -3,6 +3,11 @@
 #include "WiFiHandler.h"
 #include "ADCHandler.h"
 
+
+#include <ArduinoJson.h>
+
+int16_t value = 0;
+
 AsyncWebServer server(80);
 
 const char index_html[] PROGMEM = R"rawliteral(
@@ -37,30 +42,18 @@ void setup() {
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
       request->send_P(200, "text/html", index_html);
     });
-    server.on("/sensor", HTTP_GET, [](AsyncWebServerRequest *request) {
-      request->send(200, "text/plain", (String)adc.getADC());
+    server.onNotFound([](AsyncWebServerRequest *request)
+    {
+      request->send(404, "text/plain", "404 Not found");
     });
-    server.on("/waterable", HTTP_GET, [](AsyncWebServerRequest *request) {
-      if(waterable)
-      {
-        request->send(200, "text/plain", "TRUE");
-      }
-      else
-      {
-        request->send(200, "text/plain", "FALSE");
-      }
-    });
-
-    server.on("/water", HTTP_GET, [](AsyncWebServerRequest *request) {
-      if(waterable)
-      {
-        digitalWrite(12, HIGH);
-        request->send(200, "text/plain", "WATERED");
-      }
-      else
-      {
-        request->send(200, "text/plain", "FAILURE");
-      }
+    server.on("/sensor", HTTP_GET, [](AsyncWebServerRequest *request)
+    {
+      StaticJsonDocument<200> data;
+      String response;
+      data["sensor"] = adc.getADC();
+      data["waterable"] = waterable();
+      serializeJson(data, response);
+      request->send(200, "application/json", response);
     });
     server.begin();
   }
